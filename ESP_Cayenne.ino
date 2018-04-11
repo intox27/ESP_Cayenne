ff5833d545001e30b0d45d402f933381bd278607
intox27 <bastien.cuissard@free.fr>
2018-04-11 11:37:15 +0200

    Update
    Update some stuffs.. improve code and implement for hygrometer.

--- ESP_Cayenne.ino
+++ ESP_Cayenne.ino
@@ -9,47 +9,59 @@ char wifiPassword[] = "******";
 char username[] = "******";
 char password[] = "******";
 char clientID[] = "******";
-unsigned long lastMillis = 0;
-float h;
-float t;
 DHT dht(DHTPIN, DHTTYPE);
+float t;
+float h;
 
 void setup() {
-	
-	Serial.begin(115200);
-	pinMode(D5, OUTPUT);
-	pinMode(D6, OUTPUT);
-	pinMode(D7, OUTPUT);
-	Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+    Serial.begin(115200);
+    pinMode(D5, OUTPUT);
+    pinMode(D6, OUTPUT);
+    Cayenne.begin(username, password, clientID, ssid, wifiPassword);
 }
 
-void loop() {
-	
-	h = dht.readHumidity();
-	t = dht.readTemperature();
-	Cayenne.loop();
-	if (millis() - lastMillis > 10000) {
-		lastMillis = millis();
-		Cayenne.virtualWrite(0, lastMillis);
-		Cayenne.celsiusWrite(1, t);
-		Cayenne.celsiusWrite(2, h);
-	}
+void loop(){Cayenne.loop();}
+
+CAYENNE_OUT(0){Cayenne.virtualWrite(0, millis());}
+
+CAYENNE_OUT(1)
+{
+  do 
+  {
+    t = dht.readTemperature();
+    delay(1000);
+  }while  (isnan(t));
+  Cayenne.virtualWrite(1, t);
 }
 
-CAYENNE_IN(3)
+CAYENNE_OUT(2)
+{
+  do 
+  {
+    h = dht.readHumidity();
+    delay(1000);
+  }while  (isnan(h));
+  Cayenne.virtualWrite(2, h);
+}
+
+CAYENNE_OUT(3)
+{
+  delay(300);
+  float plant = ( 1024 - analogRead(A0) );
+  byte plantPer = float(plant / 800) * 100;  // ~800 is top range for my sensors
+  Cayenne.virtualWrite(3, plantPer);
+}
+
+CAYENNE_IN(4)
 {
   if (getValue.asInt() == 1) digitalWrite(D5, HIGH);
   else  digitalWrite(D5, LOW);
 }
-CAYENNE_IN(4)
+
+CAYENNE_IN(5)
 {
   if (getValue.asInt() == 1) digitalWrite(D6, HIGH);
   else  digitalWrite(D6, LOW);
-}
-CAYENNE_IN(5)
-{
-  if (getValue.asInt() == 1) digitalWrite(D7, HIGH);
-  else  digitalWrite(D7, LOW);
 }
 
 CAYENNE_IN_DEFAULT()
